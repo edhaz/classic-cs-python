d4711f8504896d84274bc57de0ea4fd6255e1368
Ed Hazledine <ed.hazledine@gmail.com>
2019-05-16 06:31:07 +0100

    Finished fibonacci

--- code/chapter-1/fibonacci.py
+++ code/chapter-1/fibonacci.py
@@ -1,18 +1,20 @@
-from typing import Dict
+from typing import Dict, Generator
+from functools import lru_cache
 
-# Attempt 1 - Infinite recursion
+
+# 1 - Infinite recursion
 def fib1(n: int) -> int:
     return fib1(n - 1) + fib1(n - 2)
 
 
-# Attempt 2 - recusrive with base case
+# 2 - Recursive with base case
 def fib2(n: int) -> int:
     if n < 2:  # base case
         return n
     return fib2(n - 2) + fib2(n - 1)  # recursive case
 
 
-# Memoization - store results as you create them to reuse later.
+# 3 - Memoization - store results as you create them to reuse later.
 memo: Dict[int, int] = {0: 0, 1: 1}  # our base cases
 
 def fib3(n: int) -> int:
@@ -20,7 +22,34 @@ def fib3(n: int) -> int:
         memo[n] = fib3(n - 1) + fib3(n - 2)  # memoization
     return memo[n]
 
+# 4 - Automatic memoization with lru_cache decorator
+# NOTE: lru_cache seems limited to fib(996)
+@lru_cache()
+def fib4(n: int) -> int:  # same definition as fib2()
+    if n < 2:  # base case
+        return n
+    return fib4(n - 2) + fib4(n - 1)  # recursive case
+
+
+# 5 - simple iterative approach
+def fib5(n: int) -> int:
+    if n == 0: return n  # special case
+    last: int = 0  # initially set to fib(0)
+    next: int = 1  # initially set to fib(1)
+    for _ in range(1, n):
+        last, next = next, last + next  # tuple unpacking
+    return next
+
+# 6 - using a generator
+def fib6(n: int) -> Generator[int, None, None]:
+    yield 0  # special case
+    if n > 0: yield 1  # special case
+    last: int = 0  # initially set to fib(0)
+    next: int = 1  # initially set to fib(1)
+    for _ in range(1, n):
+        last, next = next, last + next  # tuple unpacking
+        yield next  # main generation step
+
 
 if __name__ == "__main__":
-    print(fib3(40))
-    print(fib3(50))
+    print(fib4(997))
