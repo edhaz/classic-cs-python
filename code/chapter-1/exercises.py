7b604bc291775d8b06867ae9f1cbeb82b2143754
Ed Hazledine <ed.hazledine@gmail.com>
2019-06-11 20:01:57 +0100

    Fib exercise

--- code/chapter-1/exercises.py
+++ code/chapter-1/exercises.py
@@ -1,7 +1,11 @@
 #  1. Write a different version of solving fibonnaci
-
+import time
+start_time = time.time()
 
 def fib(n):
-    pass
+    memo = [0, 1]
+    for _ in range(2, n + 1):
+        memo.append(memo[_ - 1] + memo[_ - 2])
+    return memo[n]
 
-print(fib(6))
+# print("Time taken:", time.time() - start_time)
